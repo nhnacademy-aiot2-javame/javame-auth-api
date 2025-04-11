dcdaca373154d7b27bfc60f52dcecf50fc705ee5
JiwonLee-2 <this0aid@gmail.com>
2025-04-11 14:50:56 +0900

    Mock으로 수정.

--- src/test/java/com/nhnacademy/auth/AuthApplicationTests.java
+++ src/test/java/com/nhnacademy/auth/AuthApplicationTests.java
@@ -1,13 +1,17 @@
 package com.nhnacademy.auth;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("dev")
 class AuthApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
