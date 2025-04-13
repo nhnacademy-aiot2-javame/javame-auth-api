4ac331e060463130f0d0f7b753d47346aa847360
jangwonsung <jangwonsung@gmail.com>
2025-04-13 19:14:11 +0900

    auth개선 시작

--- src/main/java/com/nhnacademy/auth/service/MemberDetailService.java
+++ src/main/java/com/nhnacademy/auth/service/MemberDetailService.java
@@ -1,27 +1,27 @@
-package com.nhnacademy.auth.service;
-
-import com.nhnacademy.auth.adaptor.LoginAdaptor;
-import com.nhnacademy.auth.dto.LoginResponse;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public class MemberDetailService implements UserDetailsService {
-    /**
-     * 회원 정보를 가져오는 adaptor.
-     */
-    private final LoginAdaptor loginAdaptor;
-
-    public MemberDetailService(LoginAdaptor loginAdaptor) {
-        this.loginAdaptor = loginAdaptor;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        LoginResponse loginResponse = loginAdaptor.getLoginInfo(username).getBody();
-        if (loginResponse == null) {
-            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
-        }
-        return new MemberDetails(loginResponse);
-    }
-}
+//package com.nhnacademy.auth.service;
+//
+//import com.nhnacademy.auth.adaptor.MemberAdaptor;
+//import com.nhnacademy.auth.dto.LoginResponse;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//
+//public class MemberDetailService implements UserDetailsService {
+//    /**
+//     * 회원 정보를 가져오는 adaptor.
+//     */
+//    private final MemberAdaptor loginAdaptor;
+//
+//    public MemberDetailService(MemberAdaptor loginAdaptor) {
+//        this.loginAdaptor = loginAdaptor;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        LoginResponse loginResponse = loginAdaptor.getLoginInfo(username).getBody();
+//        if (loginResponse == null) {
+//            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+//        }
+//        return new MemberDetails(loginResponse);
+//    }
+//}
