2c1be02fc1ef7a52a4c29560caedcd182e4d17b1
JiwonLee-2 <this0aid@gmail.com>
2025-04-29 14:13:40 +0900

    feature-Redis: #6 Filter 테스트 끝.

--- src/main/java/com/nhnacademy/auth/context/ApplicationContextHolder.java
+++ src/main/java/com/nhnacademy/auth/context/ApplicationContextHolder.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.auth.context;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -11,6 +12,7 @@ public class ApplicationContextHolder implements ApplicationContextAware {
      * 직접 Bean 주입을 안 하고 전역적으로 스프링 컨텍스트를 잡을 컨텍스트입니다.
      */
     @Getter
+    @Setter
     private static ApplicationContext context;
 
     @Override
