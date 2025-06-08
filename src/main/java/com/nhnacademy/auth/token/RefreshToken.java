237b9cf1e860426cfb3bb1e7bb114d327aef29a2
JiwonLee-2 <this0aid@gmail.com>
2025-06-08 17:08:15 +0900

    feature/modify2: #21
    accessToken 수정.

--- src/main/java/com/nhnacademy/auth/token/RefreshToken.java
+++ src/main/java/com/nhnacademy/auth/token/RefreshToken.java
@@ -8,7 +8,7 @@ import org.springframework.data.redis.core.RedisHash;
 
 @ToString
 @EqualsAndHashCode
-@RedisHash(value = "refreshToken", timeToLive = 604800)
+@RedisHash(value = "refreshToken", timeToLive = 86400)
 public class RefreshToken {
     /**
      * Redis에 담길 RefreshToken의 Key값입니다.
@@ -21,7 +21,7 @@ public class RefreshToken {
      *  Redis에 담길 RefreshToken의 value값 입니다.
      */
     @JsonProperty
-    private String refreshToken;
+    private String token;
 
     /**
      * 사용자를 대신하여 작업을 수행하는 소프트웨어. 특히 웹 브라우저 같은 클라이언트 소프트웨어를 의미합니다.
@@ -40,9 +40,9 @@ public class RefreshToken {
         // NoArgsConstructor
     }
 
-    public RefreshToken(String id, String refreshToken, String userAgent, String ip) {
+    public RefreshToken(String id, String token, String userAgent, String ip) {
         this.id = id;
-        this.refreshToken = refreshToken;
+        this.token = token;
         this.userAgent = userAgent;
         this.ip = ip;
     }
@@ -59,7 +59,7 @@ public class RefreshToken {
         return ip;
     }
 
-    public String getRefreshToken() {
-        return refreshToken;
+    public String getToken() {
+        return token;
     }
 }
